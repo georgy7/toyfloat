7b8fb8c979b025f424ae921e6a4e512d6447c852
Georgy Ustinov <httpcatharsis@gmail.com>
2021-12-19 07:45:37 +0300

    Pair tests: SetA, SetB

--- pkg/usage/pair_test.go
+++ pkg/usage/pair_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestPair(t *testing.T) {
+func TestNewPair(t *testing.T) {
 	tests := []struct {
 		a, b, eps float64
 	}{
@@ -18,10 +18,14 @@ func TestPair(t *testing.T) {
 		{-234, 128, 0.5},
 		{234, -128, 0.5},
 		{-234, -128, 0.5},
+		{-235, -128, 0.5},
+		{500, -128, 1.0},
+		{501, -128, 1.0},
+		{502, -128, 1.0},
 	}
 
 	for ti, tt := range tests {
-		testName := fmt.Sprintf("pair #%d", ti)
+		testName := fmt.Sprintf("NewPair #%d", ti)
 		t.Run(testName, func(t *testing.T) {
 			got, e := NewPair(tt.a, tt.b)
 			if e != nil {
@@ -34,6 +38,82 @@ func TestPair(t *testing.T) {
 
 			if math.Abs(got.B()-tt.b) > tt.eps {
 				t.Errorf("B(): %.4f != %.4f", got.B(), tt.b)
+			}
+		})
+	}
+}
+
+func TestPairSetA(t *testing.T) {
+	pair, e := NewPair(0, 0)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	tests := []struct {
+		v, eps float64
+	}{
+		{-345, 1.0},
+		{-145, 0.5},
+		{-1, 0.01},
+		{-0.5, 0.01},
+		{-0.34, 0.01},
+		{0, 0.01},
+		{0.34, 0.01},
+		{0.5, 0.01},
+		{1, 0.01},
+		{145, 0.5},
+		{345, 1.0},
+	}
+
+	for ti, tt := range tests {
+		testName := fmt.Sprintf("SetA #%d", ti)
+		t.Run(testName, func(t *testing.T) {
+			pair.SetA(tt.v)
+
+			if math.Abs(pair.A()-tt.v) > tt.eps {
+				t.Errorf("%.4f != %.4f", pair.A(), tt.v)
+			}
+
+			if math.Abs(pair.B()) > 0.01 {
+				t.Errorf("changed B (%.4f)", pair.B())
+			}
+		})
+	}
+}
+
+func TestPairSetB(t *testing.T) {
+	pair, e := NewPair(0, 0)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	tests := []struct {
+		v, eps float64
+	}{
+		{-345, 1.0},
+		{-145, 0.5},
+		{-1, 0.01},
+		{-0.5, 0.01},
+		{-0.34, 0.01},
+		{0, 0.01},
+		{0.34, 0.01},
+		{0.5, 0.01},
+		{1, 0.01},
+		{145, 0.5},
+		{345, 1.0},
+	}
+
+	for ti, tt := range tests {
+		testName := fmt.Sprintf("SetA #%d", ti)
+		t.Run(testName, func(t *testing.T) {
+			pair.SetB(tt.v)
+
+			if math.Abs(pair.B()-tt.v) > tt.eps {
+				t.Errorf("%.4f != %.4f", pair.B(), tt.v)
+			}
+
+			if math.Abs(pair.A()) > 0.01 {
+				t.Errorf("changed A (%.4f)", pair.A())
 			}
 		})
 	}
