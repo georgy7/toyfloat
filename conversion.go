c6534eff8373bcef433ab33d64a1598655c3d2b0
Georgy Ustinov <httpcatharsis@gmail.com>
2022-01-06 23:03:56 +0300

    Refactoring. CPU optimization.

--- conversion.go
+++ conversion.go
@@ -9,113 +9,66 @@ import (
 
 // Type is a reusable immutable set of encoder settings.
 type Type struct {
-	settings settings
+	mSize        int
+	minus        uint16
+	mMask        uint16
+	innerMaximum float64
+	twoPowerM    float64
+	xc           xConstants
 }
 
 func NewTypeX2(length int, signed bool) (Type, error) {
-	s, e := newSettings(length, x2(), signed)
-	return Type{s}, e
+	return newSettings(length, 2, -3, true, signed)
 }
 
 func NewTypeX3(length int, signed bool) (Type, error) {
-	s, e := newSettings(length, x3(), signed)
-	return Type{s}, e
+	return newSettings(length, 3, -6, false, signed)
 }
 
 func NewTypeX4(length int, signed bool) (Type, error) {
-	s, e := newSettings(length, x4(), signed)
-	return Type{s}, e
+	return newSettings(length, 4, -8, false, signed)
 }
 
 func (t *Type) Encode(v float64) uint16 {
-	return encode(v, &t.settings)
+	return encode(v, t)
 }
 
 func (t *Type) Decode(x uint16) float64 {
-	return decode(x, &t.settings)
+	return decode(x, t)
 }
 
 func (t *Type) GetIntegerDelta(last uint16, x uint16) int {
-	return encodeDelta(last, x, &t.settings)
+	return encodeDelta(last, x, t)
 }
 
 func (t *Type) UseIntegerDelta(last uint16, delta int) uint16 {
-	return decodeDelta(last, delta, &t.settings)
+	return decodeDelta(last, delta, t)
 }
 
 // ----------------
 // Implementation:
 
 type xConstants struct {
-	xMask                uint16
-	xSize                int
-	minExponent          int
-	maxExponent          int
-	base3                bool
-	basePowerMinExponent float64
-	basePowerMaxExponent float64
+	xMask       uint16
+	xSize       int
+	minExponent int
+	maxExponent int
+	base3       bool
+	scales      [16]float64
 }
 
-type settings struct {
-	mSize  int
-	xShift int
-	minus  uint16
-	mMask  uint16
-	xc     xConstants
-}
-
-func x4() xConstants {
-	return xConstants{
-		xMask:                0b1111,
-		xSize:                4,
-		minExponent:          -8,
-		maxExponent:          -8 + 15,
-		base3:                false,
-		basePowerMinExponent: 1.0 / 256.0,
-		basePowerMaxExponent: 128.0,
-	}
-}
-
-func x3() xConstants {
-	return xConstants{
-		xMask:                0b111,
-		xSize:                3,
-		minExponent:          -6,
-		maxExponent:          -6 + 7,
-		base3:                false,
-		basePowerMinExponent: 1.0 / 64.0,
-		basePowerMaxExponent: 2.0,
-	}
-}
-
-func x2() xConstants {
-	return xConstants{
-		xMask:                0b11,
-		xSize:                2,
-		minExponent:          -3,
-		maxExponent:          0,
-		base3:                true,
-		basePowerMinExponent: 1.0 / 27.0,
-		basePowerMaxExponent: 1.0,
-	}
-}
-
-func makeSettings(mSize int, minus, mMask uint16, xc xConstants) settings {
-	return settings{mSize, mSize, minus, mMask, xc}
-}
-
-func newSettings(length int, xc xConstants, signed bool) (settings, error) {
+func newSettings(length int, xSize, minX int, b3, signed bool) (Type, error) {
 	if length > 16 {
-		return settings{}, errors.New("maximum length is 16 bits")
+		return Type{}, errors.New("maximum length is 16 bits")
 	}
 
-	mSize := length - xc.xSize
+	mSize := length - xSize
 	if signed {
 		mSize -= 1
 	}
 
 	if mSize < 1 {
-		return settings{}, errors.New("mantissa must be at least 1 bit wide")
+		return Type{}, errors.New("mantissa must be at least 1 bit wide")
 	}
 
 	minus := uint16(0)
@@ -123,57 +76,79 @@ func newSettings(length int, xc xConstants, signed bool) (settings, error) {
 		minus = uint16(1) << (length - 1)
 	}
 
-	mMask := makeBitMask(mSize)
-	return makeSettings(mSize, minus, mMask, xc), nil
+	mMask := getOnes(mSize)
+	maxX := minX + (int(1) << xSize) - 1
+
+	if xSize > 4 {
+		return Type{}, errors.New("such big exponents are not supported")
+	}
+
+	var scales [16]float64
+	for x := minX; x <= maxX; x++ {
+		scales[x-minX] = calculateScale(b3, x)
+	}
+
+	twoPowerM := powerOfTwo(mSize)
+	mMax := twoPowerM - 1.0
+
+	internalMaximum := decodeSignificand(mMax, twoPowerM, b3)
+	internalMaximum *= calculateScale(b3, maxX)
+
+	return Type{mSize, minus, mMask,
+		internalMaximum,
+		twoPowerM,
+		xConstants{
+			xMask:       getOnes(xSize),
+			xSize:       xSize,
+			minExponent: minX,
+			maxExponent: maxX,
+			base3:       b3,
+			scales:      scales,
+		}}, nil
 }
 
-func isNegative(tf uint16, settings *settings) bool {
+func isNegative(tf uint16, settings *Type) bool {
 	return 0b0 != tf&(settings.minus)
 }
 
-func abs(tf uint16, settings *settings) uint16 {
+func abs(tf uint16, settings *Type) uint16 {
 	return tf & (^settings.minus)
 }
 
-func encode(value float64, settings *settings) uint16 {
-
+func encode(value float64, settings *Type) uint16 {
 	if math.IsNaN(value) {
 		return 0x0
 	}
 
-	a := settings.xc.basePowerMinExponent
-	reversedC := 1.0 - a
+	nv := value < 0
 
-	if value >= 0 {
-		return encodeInnerValue(value*reversedC+a, settings)
-	} else if 0b0 == settings.minus {
+	if nv && (0b0 == settings.minus) {
 		return 0x0
+	}
+
+	a := settings.xc.scales[0]
+	vReversedC := value * (1.0 - a)
+
+	if nv {
+		return settings.minus | encodeInnerValue(a-vReversedC, settings)
 	} else {
-		return settings.minus | encodeInnerValue(-value*reversedC+a, settings)
+		return encodeInnerValue(a+vReversedC, settings)
 	}
 }
 
-func decode(tf uint16, settings *settings) float64 {
+func decode(tf uint16, settings *Type) float64 {
+	a := settings.xc.scales[0]
+	c := 1.0 / (1.0 - a)
 
-	exponentOffset := -settings.xc.minExponent
-	a := settings.xc.basePowerMinExponent
-	reversedC := 1.0 - a
-	c := 1.0 / reversedC
+	xShift, xMask := settings.mSize, settings.xc.xMask
+	mMask := settings.mMask
 
-	xShift, xMask := settings.xShift, settings.xc.xMask
-	mMask, mSize := settings.mMask, settings.mSize
+	x := int((tf>>xShift)&xMask) + settings.xc.minExponent
 
-	x := int((tf>>xShift)&xMask) - exponentOffset
+	significand := decodeSignificand(
+		float64(tf&mMask), settings.twoPowerM, settings.xc.base3)
 
-	significand := float64(tf&mMask) / powerOfTwo(mSize)
-	if settings.xc.base3 {
-		significand *= 2.0
-	}
-	significand += 1.0
-
-	scale := getScale(settings, x)
-
-	r := significand * scale
+	r := significand * getScale(x, settings)
 	r = (r - a) * c
 
 	// The problem only appeared with base three exponent.
@@ -181,60 +156,59 @@ func decode(tf uint16, settings *settings) float64 {
 		r = 1.0
 	}
 
-	return r * sign(tf, settings.minus)
+	if 0b0 != tf&settings.minus {
+		return -r
+	}
+
+	return r
 }
 
-func sign(x uint16, minus uint16) float64 {
-	if 0b0 == x&minus {
-		return 1
-	} else {
-		return -1
-	}
-}
+func encodeInnerValue(inner float64, s *Type) uint16 {
 
-func encodeInnerValue(inner float64, s *settings) uint16 {
+	xShift, xMask := s.mSize, s.xc.xMask
+	xBias := s.xc.minExponent
 
-	xShift, xMask := s.xShift, s.xc.xMask
-	mMask, mSize := s.mMask, s.mSize
+	mMax := s.twoPowerM - 1.0
 
-	exponentOffset := -s.xc.minExponent
-	maxValue := (xMask << xShift) | mMask
-
-	twoPowerM := powerOfTwo(mSize)
-	mMax := twoPowerM - 1.0
-
-	internalMaximum := mMax / twoPowerM
-	if s.xc.base3 {
-		internalMaximum *= 2.0
-	}
-	internalMaximum += 1.0
-	internalMaximum *= s.xc.basePowerMaxExponent
-
-	if inner >= internalMaximum {
-		return maxValue
+	if inner >= s.innerMaximum {
+		return (xMask << xShift) | s.mMask
 	}
 
-	x := getExponent(inner, s)
-	binaryExponent := uint16(x+exponentOffset) << xShift
+	x, scale := getExponent(inner, s)
+	binaryExponent := uint16(x-xBias) << xShift
 
-	scale := getScale(s, x)
 	normalized := inner / scale
 
-	mFloat := normalized - 1.0
-	if s.xc.base3 {
-		mFloat *= 0.5
-	}
-	mFloat *= twoPowerM
+	mFloat := encodeSignificand(normalized, s)
 
 	binarySignificand := uint16(math.Min(math.Round(mFloat), mMax))
 
 	return binarySignificand | binaryExponent
 }
 
-func getExponent(innerValue float64, s *settings) int {
+func encodeSignificand(normalized float64, s *Type) float64 {
+	mFloat := normalized - 1.0
+	if s.xc.base3 {
+		return mFloat * powerOfTwo(s.mSize-1)
+	} else {
+		return mFloat * s.twoPowerM
+	}
+}
+
+func decodeSignificand(m, twoPowerM float64, base3 bool) float64 {
+	r := m
+	if base3 {
+		r /= twoPowerM * 0.5
+	} else {
+		r /= twoPowerM
+	}
+	return 1.0 + r
+}
+
+func getExponent(innerValue float64, s *Type) (int, float64) {
 	modulus := math.Abs(innerValue)
 
-	eps := 0.5 / powerOfTwo(s.mSize)
+	eps := 0.5 / s.twoPowerM
 	var factor float64
 
 	if s.xc.base3 {
@@ -243,16 +217,21 @@ func getExponent(innerValue float64, s *settings) int {
 		factor = 2.0 - eps
 	}
 
+	scale := getScale(s.xc.maxExponent, s)
+
 	for exp := s.xc.maxExponent; exp > s.xc.minExponent; exp-- {
-		if factor*getScale(s, exp-1) <= modulus {
-			return exp
+		smallerScale := getScale(exp-1, s)
+		if factor*smallerScale <= modulus {
+			return exp, scale
+		} else {
+			scale = smallerScale
 		}
 	}
 
-	return s.xc.minExponent
+	return s.xc.minExponent, scale
 }
 
-func makeBitMask(bits int) uint16 {
+func getOnes(bits int) uint16 {
 	count := bits
 	if count > 16 {
 		count = 16
@@ -277,14 +256,41 @@ func powerOfTwo(x int) float64 {
 }
 
 func powerOfThree(x int) float64 {
-	return math.Pow(3.0, float64(x))
+	root := x < 0
+
+	var absX int
+	if root {
+		absX = -x
+	} else {
+		absX = x
+	}
+
+	rInt := 1
+	for i := 0; i < absX; i++ {
+		rInt *= 3
+	}
+
+	if root {
+		return 1.0 / float64(rInt)
+	} else {
+		return float64(rInt)
+	}
 }
 
-func getScale(s *settings, x int) float64 {
-	if s.xc.base3 {
+func calculateScale(base3 bool, x int) float64 {
+	if base3 {
 		return powerOfThree(x)
 	} else {
 		return powerOfTwo(x)
+	}
+}
+
+func getScale(x int, s *Type) float64 {
+	index := x - s.xc.minExponent
+	if index <= 0 {
+		return s.xc.scales[0]
+	} else {
+		return s.xc.scales[index%len(s.xc.scales)]
 	}
 }
 
@@ -296,7 +302,7 @@ func min(a int, b int) int {
 	}
 }
 
-func encodeDelta(last uint16, x uint16, settings *settings) int {
+func encodeDelta(last uint16, x uint16, settings *Type) int {
 	lastIsNegative := isNegative(last, settings)
 	xIsNegative := isNegative(x, settings)
 	sameSign := lastIsNegative == xIsNegative
@@ -322,14 +328,14 @@ func encodeDelta(last uint16, x uint16, settings *settings) int {
 	}
 }
 
-func decodeDelta(last uint16, delta int, s *settings) uint16 {
+func decodeDelta(last uint16, delta int, s *Type) uint16 {
 	if delta == 0 {
 		return last
 	}
 
 	absLast := int(abs(last, s))
 
-	xShift, xMask := s.xShift, s.xc.xMask
+	xShift, xMask := s.mSize, s.xc.xMask
 	mMask := s.mMask
 	maxValue := (xMask << xShift) | mMask
 
